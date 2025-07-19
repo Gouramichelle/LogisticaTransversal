10d8c6d097286a6063459666994cc6b045deea1a
GR <gouramichelle24@gmail.com>
2025-07-18 22:44:14 -0400

    commit

--- src/main/java/com/ecomarket/logistica/service/UsuarioService.java
+++ src/main/java/com/ecomarket/logistica/service/UsuarioService.java
@@ -1,4 +1,22 @@
 package com.ecomarket.logistica.service;
 
+import com.ecomarket.logistica.model.UsuarioDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+
 public class UsuarioService {
+    private RestTemplate restTemplate;
+    @Value("${services.usuario.url}")
+    private String usuarioUrl;
+    public UsuarioService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public UsuarioDTO obtenerUsuarioId(Integer id) {
+        String url = this.usuarioUrl + "/" + id;
+        return restTemplate.getForObject(url, UsuarioDTO.class);
+    }
 }

498c56c25a04685709ed9c9d03e01cf1abe08a3b
KarolGiraldo <151104196+KarcatBit@users.noreply.github.com>
2025-07-18 23:09:44 -0400

    FacturacionService

--- src/main/java/com/ecomarket/logistica/service/FacturacionService.java
+++ src/main/java/com/ecomarket/logistica/service/FacturacionService.java
@@ -1,4 +1,24 @@
 package com.ecomarket.logistica.service;
 
+import com.ecomarket.logistica.model.FacturacionDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class FacturacionService {
+
+    private final RestTemplate restTemplate;
+
+    @Value("${services.facturacion.url}") 
+    private String facturacionUrl;
+
+    public FacturacionService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public FacturacionDTO obtenerFacturacionPorId(Integer id) {
+        String url = facturacionUrl + "/" + id;
+        return restTemplate.getForObject(url, FacturacionDTO.class);
+    }
 }
